d6fa58cf2dcbcd6f0967d243b5103e0e167c8564
biohazardpb4 <prombus@gmail.com>
2018-11-16 03:48:10 +0000

    adds impl of RMSE

--- test/tools.cpp
+++ test/tools.cpp
@@ -9,12 +9,34 @@ TEST(RMSE, CanBeCalculated)
 {
 	vector<VectorXd> estimations;
 	vector<VectorXd> ground_truth;
+
+	//the input list of estimations
+	VectorXd e(4);
+	e << 1, 1, 0.2, 0.1;
+	estimations.push_back(e);
+	e << 2, 2, 0.3, 0.2;
+	estimations.push_back(e);
+	e << 3, 3, 0.4, 0.3;
+	estimations.push_back(e);
+
+	//the corresponding list of ground truth values
+	VectorXd g(4);
+	g << 1.1, 1.1, 0.3, 0.2;
+	ground_truth.push_back(g);
+	g << 2.1, 2.1, 0.4, 0.3;
+	ground_truth.push_back(g);
+	g << 3.1, 3.1, 0.5, 0.4;
+	ground_truth.push_back(g);
+
 	Tools tools;
 	
 	auto got = tools.CalculateRMSE(estimations, ground_truth);
         VectorXd want(4);
-	want << 0,0,0,0;
-	EXPECT_EQ(got, want);
+	want << 0.1, 0.1, 0.1, 0.1;
+	EXPECT_FLOAT_EQ(got[0], want[0]);
+	EXPECT_FLOAT_EQ(got[1], want[1]);
+	EXPECT_FLOAT_EQ(got[2], want[2]);
+	EXPECT_FLOAT_EQ(got[3], want[3]);
 }
 
 int main(int argc, char **argv) {
